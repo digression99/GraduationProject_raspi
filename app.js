527dd84b3e78e0b760e964f088f1293397a0084d
Kim Ilsik <digression99@gmail.com>
2017-08-23 20:14:11 +0900

    20170823 first commit

--- app.js
+++ app.js
@@ -2,19 +2,101 @@ const express = require('express');
 var Raspi = require('raspi-io');
 const five = require('johnny-five');
 const gpio = require('raspi-gpio');
+var RaspiCam = require('raspicam');
+var request = require('request');
+var fs = require('fs');
+var path = require('path');
 
+const app = express();
+
+var headers = {
+	'Content-Type' : 'application/json'
+};
+
+var opts = {
+	width : 600,
+	height : 420,
+	mode : 'timelapse',
+	awb : 'off',
+	encoding : 'jpg',
+	output : 'images/camera.jpg',
+	q : 50,
+	timeout : 1000,
+	timelapse : 0,
+	nopreview : true,
+	th : '0:0:0'
+};
+
+var camera = new RaspiCam(opts);
+
+// camera event handler.
+camera.on('exit', function() {
+	camera.stop();
+	console.log('camera stopped.');
+	fs.readFile(path.join(__dirname, 'images/camera.jpg'), function(err, img) {
+		if (err) console.log(err);
+		else {
+			console.log('in readFile.');
+			var imgJson = img.toJSON();
+			var date = new Date();
+			var nowDate = date.getFullYear() + '-' + date.getMonth() + '-' + date.getDay();
+			console.log('data set');
+			
+			var formData = {
+				img : imgJson,
+				date : nowDate
+			};
+			/*
+			request.post({url:'http://pseudocoder.rocks/api.face', formData : formData},
+				function(err, httpResponse, body) {
+					if (err) console.log(err);
+					else {console.log('response : ', body);
+					}
+			});
+			*/
+
+			
+			var options = {
+				url : 'http://www.pseudocoder.rocks/api/face',
+				method : 'POST',
+				headers : headers,
+				json : true,
+				body : formData
+			};
+			
+			console.log('options set');
+			request(options, function(err, res, body) {
+				console.log('in request');
+				if (err) console.log(err);
+				else {
+					if (res.statusCode === 200) {
+						console.log('successfully trasmitted.');
+						console.log(JSON.stringify(body));
+					} else {
+						console.log('something wrong.', res.statusCode);
+					}
+				}
+			});
+			
+		}		
+
+
+	});
+});
+
+// board setting.
 const board = new five.Board({
 	io : new Raspi()
 });
 
 board.on('ready', function() {
+	console.log('board is ready!');
 	(new five.Led('P1-7')).strobe();
 });
 
-const app = express();
-
 app.get('/', function(req, res) {
-	res.send('Hello, raspberry pi!');
+	camera.start();
+	res.send('Hello, raspberry pi!, camera started.');
 });
 
 app.listen(3000, function(req, res) {
