d525e5917ec67b39e4c4099a2e63e639b4994098
Kim Il Sik <kimilsik@Kims-MacBook.local>
2018-01-09 23:47:05 +0900

    board test

---
+++ playground/board-test.js
@@ -0,0 +1,12 @@
+var Raspi = require('raspi-io');
+const five = require('johnny-five');
+
+// board setting.
+const board = new five.Board({
+    io : new Raspi()
+});
+
+board.on('ready', function() {
+    console.log('board is ready!');
+    (new five.Led('P1-7')).strobe();
+});
