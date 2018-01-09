7bcfcfb58fc3a10d1b5b125395c97ebdf785b846
Kim Il Sik <kimilsik@Kims-MacBook.local>
2018-01-09 23:36:36 +0900

    test pify 1

--- playground/pify-test-1.js
+++ playground/pify-test-1.js
@@ -1,1 +1,15 @@
-console.log('integ complete!');
+const fs = require('fs');
+const pify = require('pify');
+const path = require('path');
+
+const getData = async () => {
+    const data = await pify(fs).readFile(path.join(__dirname, 'setting.json'));
+
+    console.log(JSON.parse(data));
+    return data;
+};
+
+getData();
+
+
+// const data = await pify(fs).readFile(path.join(__dirname, 'setting.json'), 'utf-8')
