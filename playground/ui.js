c9cc00220c379df06ade8ab58669040ce42c0887
Kim Il Sik <kimilsik@Kims-MacBook.local>
2018-01-11 19:47:17 +0900

    button test

--- playground/ui.js
+++ playground/ui.js
@@ -68,7 +68,7 @@ class UI {
     }
 
     onSelect() {
-        if (this.state.node.children !== []) {
+        if (this.state.node.children) {
             console.log('NOT nothing!');
             this.state.node = this.state.node.children[this.state.select];
             this.state.select = 0;
