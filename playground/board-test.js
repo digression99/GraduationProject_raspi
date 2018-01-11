cc41e2605b658e63c0759aea730b60e4f96108fc
Kim Il Sik <kimilsik@Kims-MacBook.local>
2018-01-11 20:01:46 +0900

    button test

--- playground/board-test.js
+++ playground/board-test.js
@@ -1,276 +1,284 @@
 let Raspi = require('raspi-io');
 const five = require('johnny-five');
+const {initCli, saveCli} = require('./utils');
 
-let ui = require('./ui');
+let todos, setting;
 let select = 0;
 let isHold = false;
 
-// board setting.
-const board = new five.Board({
-    io : new Raspi()
-});
+initCli()
+    .then(({initTodos, initSetting}) => {
+        todos = initTodos;
+        setting = initSetting;
+        let ui = require('./ui')(todos, setting);
 
-board.on('event-start', function(data) {
-    console.log('event-start!');
-    console.log(data.name);
-});
+        // board setting.
+        const board = new five.Board({
+            io : new Raspi()
+        });
 
-board.on('button-click', (data) => {
-    switch (data.action) {
-        case 'left':
-        case 'right':
-            const offset = data.action === 'left' ? -1 : 1;
-            ui.changeSelect(offset);
-            break;
-        case 'select':
-            ui.onSelect();
-            break;
-        case 'back':
-            ui.onBack();
-            break;
-        default:
-            console.log('error');
-            break;
-    }
-    ui.onAction();
-    console.log(ui.state.select);
-});
+        board.on('event-start', function(data) {
+            console.log('event-start!');
+            console.log(data.name);
+        });
+
+        board.on('button-click', (data) => {
+            switch (data.action) {
+                case 'left':
+                case 'right':
+                    const offset = data.action === 'left' ? -1 : 1;
+                    ui.changeSelect(offset);
+                    break;
+                case 'select':
+                    ui.onSelect();
+                    break;
+                case 'back':
+                    ui.onBack();
+                    break;
+                default:
+                    console.log('error');
+                    break;
+            }
+            ui.onAction();
+            console.log(ui.state.select);
+        });
 
 // board.on('event-exit', function() {
 //     console.log('event exit!');
 // });
 
-board.on('exit', function() {
-    console.log('exit!');
-});
-
-board.on('ready', function() {
-    console.log('board is ready!');
-    ui.onAction();
-
-    // let digitalLed1 = new five.Led('P1-7');
-    // let digitalLed2 = new five.Led('P1-11');
-    let button1 = new five.Button('P1-13');
-    // let switchButton = new five.Switch('P1-15');
-    let button2 = new five.Button('P1-16');
-    // let motion = new five.Motion('P1-12');
-
-    let button3 = new five.Button('P1-31');
-    // let button4 = new five.Button('P1-37');
-    // let button5 = new five.Button({
-    //     pin : 'P1-18',
-    //     holdtime : 1000
-    // });
-
-    this.repl.inject({
-        button1,
-        button2,
-        button3
-    });
-
-    button1.on('down', () => {
-        // console.log('button 1 down');
-        board.emit('button-click', {
-            action : 'left'
+        board.on('exit', function() {
+            console.log('exit!');
         });
-    });
-    button1.on('hold', () => {
-        // console.log('button 1 hold');
-    });
-    button1.on('up', () => {
-        // console.log('button 1 up');
-    });
 
-    button2.on('down', () => {
-        // console.log('button 2 down');
-    });
-    button2.on('hold', () => {
-        // console.log('button 2 hold');
-        // board.emit('button-click', {
-        //     action : ''
-        // });
-        isHold = true;
-    });
-    button2.on('up', () => {
-        if (isHold) {
-            isHold = false;
-            board.emit('button-click', {
-                action : 'back'
+        board.on('ready', function() {
+            console.log('board is ready!');
+            ui.onAction();
+
+            // let digitalLed1 = new five.Led('P1-7');
+            // let digitalLed2 = new five.Led('P1-11');
+            let button1 = new five.Button('P1-13');
+            // let switchButton = new five.Switch('P1-15');
+            let button2 = new five.Button('P1-16');
+            // let motion = new five.Motion('P1-12');
+
+            let button3 = new five.Button('P1-31');
+            // let button4 = new five.Button('P1-37');
+            // let button5 = new five.Button({
+            //     pin : 'P1-18',
+            //     holdtime : 1000
+            // });
+
+            this.repl.inject({
+                button1,
+                button2,
+                button3
             });
-        } else {
-            board.emit('button-click', {
-                action : 'select'
+
+            button1.on('down', () => {
+                // console.log('button 1 down');
+                board.emit('button-click', {
+                    action : 'left'
+                });
             });
-        }
-    });
+            button1.on('hold', () => {
+                // console.log('button 1 hold');
+            });
+            button1.on('up', () => {
+                // console.log('button 1 up');
+            });
 
-    button3.on('down', () => {
-        // console.log('button 3 down');
-        board.emit('button-click', {
-            action : 'right'
+            button2.on('down', () => {
+                // console.log('button 2 down');
+            });
+            button2.on('hold', () => {
+                // console.log('button 2 hold');
+                // board.emit('button-click', {
+                //     action : ''
+                // });
+                isHold = true;
+            });
+            button2.on('up', () => {
+                if (isHold) {
+                    isHold = false;
+                    board.emit('button-click', {
+                        action : 'back'
+                    });
+                } else {
+                    board.emit('button-click', {
+                        action : 'select'
+                    });
+                }
+            });
+
+            button3.on('down', () => {
+                // console.log('button 3 down');
+                board.emit('button-click', {
+                    action : 'right'
+                });
+            });
+            button3.on('hold', () => {
+                // console.log('button 3 hold');
+            });
+            button3.on('up', () => {
+                // console.log('button 3 up');
+            });
+
+
+
+
+
+            /*
+
+            // let analogInput = new five.Light(`P1-12`);
+
+            switchButton.isOpened = false;
+
+            this.repl.inject({
+                onLed1 : function() {
+                    digitalLed1.on();
+                    console.log('led 1 on');
+                },
+                onLed2 : function() {
+                    digitalLed2.on();
+                    console.log('led 2 on');
+                },
+                offLed1 : function() {
+                    digitalLed1.off();
+                    console.log('led 1 off');
+                },
+                offLed2 : function() {
+                    digitalLed2.off();
+                    console.log('led 2 off');
+                },
+                button : button,
+                button2 : button2,
+                switchButton : switchButton,
+                button3 : button3,
+                button4 : button4,
+                button5 : button5
+                // analogInput : analogInput
+                // motion : motion
+            });
+
+            // "calibrated" occurs once, at the beginning of a session,
+            // motion.on("calibrated", function() {
+            //     console.log("calibrated");
+            // });
+            //
+            // // "motionstart" events are fired when the "calibrated"
+            // // proximal area is disrupted, generally by some form of movement
+            // motion.on("motionstart", function() {
+            //     console.log("motionstart");
+            // });
+            //
+            // // "motionend" events are fired following a "motionstart" event
+            // // when no movement has occurred in X ms
+            // motion.on("motionend", function() {
+            //     console.log("motionend");
+            // });
+
+            switchButton.on('open', function() {
+                if (!this.isOpened) {
+                    this.isOpened = true;
+                    console.log('switch open');
+                }
+            });
+
+            switchButton.on('close', function() {
+                if (this.isOpened) {
+                    this.isOpened = false;
+                    console.log('switch close');
+                }
+            });
+
+            // "down" the button is pressed
+            button.on("down", function() {
+                console.log("down");
+                board.emit('event-start', {
+                    name : "kim"
+                });
+                digitalLed1.toggle();
+                digitalLed2.toggle();
+            });
+
+            // "hold" the button is pressed for specified time.
+            //        defaults to 500ms (1/2 second)
+            //        set
+            button.on("hold", function() {
+                // console.log("hold");
+            });
+
+            // "up" the button is released
+            button.on("up", function() {
+                // console.log("up");
+            });
+
+            button2.on("down", function() {
+                console.log("button 2 down");
+                digitalLed1.toggle();
+                digitalLed2.toggle();
+            });
+
+            // "hold" the button is pressed for specified time.
+            //        defaults to 500ms (1/2 second)
+            //        set
+            button2.on("hold", function() {
+                console.log("button 2 hold");
+            });
+
+            // "up" the button is released
+            button2.on("up", function() {
+                console.log("button 2 up");
+            });
+
+            button3.on("down", function() {
+                console.log("button 3 down");
+            });
+            button3.on("hold", function() {
+                console.log("button 3 hold");
+            });
+            button3.on("up", function() {
+                console.log("button 3 up");
+            });
+
+            button4.on("down", function() {
+                console.log("button 4 down");
+            });
+            button4.on("hold", function() {
+                console.log("button 4 hold");
+            });
+            button4.on("up", function() {
+                console.log("button 4 up");
+            });
+
+            button5.on("down", function() {
+                console.log("button 5 down");
+            });
+            button5.on("hold", function() {
+                console.log("button 5 hold");
+            });
+            button5.on("up", function() {
+                console.log("button 5 up");
+            });
+
+            // let isAnaloged = false;
+            // analogInput.on('data', function() {
+            //     if (!isAnaloged) {
+            //         console.log('data is coming!');
+            //         console.log(this);
+            //         isAnaloged = true;
+            //     }
+            // })
+
+
+            // digitalLed1.strobe();
+            // digitalLed2.strobe();
+            // digitalLed1.blink();
+            // digitalLed2.blink();
+
+            // (new five.Led('P1-7').Led('P1-8')).strobe();
+             */
+
         });
-    });
-    button3.on('hold', () => {
-        // console.log('button 3 hold');
-    });
-    button3.on('up', () => {
-        // console.log('button 3 up');
-    });
-
-
-
-
-
-    /*
-
-    // let analogInput = new five.Light(`P1-12`);
-
-    switchButton.isOpened = false;
-
-    this.repl.inject({
-        onLed1 : function() {
-            digitalLed1.on();
-            console.log('led 1 on');
-        },
-        onLed2 : function() {
-            digitalLed2.on();
-            console.log('led 2 on');
-        },
-        offLed1 : function() {
-            digitalLed1.off();
-            console.log('led 1 off');
-        },
-        offLed2 : function() {
-            digitalLed2.off();
-            console.log('led 2 off');
-        },
-        button : button,
-        button2 : button2,
-        switchButton : switchButton,
-        button3 : button3,
-        button4 : button4,
-        button5 : button5
-        // analogInput : analogInput
-        // motion : motion
-    });
-
-    // "calibrated" occurs once, at the beginning of a session,
-    // motion.on("calibrated", function() {
-    //     console.log("calibrated");
-    // });
-    //
-    // // "motionstart" events are fired when the "calibrated"
-    // // proximal area is disrupted, generally by some form of movement
-    // motion.on("motionstart", function() {
-    //     console.log("motionstart");
-    // });
-    //
-    // // "motionend" events are fired following a "motionstart" event
-    // // when no movement has occurred in X ms
-    // motion.on("motionend", function() {
-    //     console.log("motionend");
-    // });
-
-    switchButton.on('open', function() {
-        if (!this.isOpened) {
-            this.isOpened = true;
-            console.log('switch open');
-        }
-    });
-
-    switchButton.on('close', function() {
-        if (this.isOpened) {
-            this.isOpened = false;
-            console.log('switch close');
-        }
-    });
-
-    // "down" the button is pressed
-    button.on("down", function() {
-        console.log("down");
-        board.emit('event-start', {
-            name : "kim"
-        });
-        digitalLed1.toggle();
-        digitalLed2.toggle();
-    });
-
-    // "hold" the button is pressed for specified time.
-    //        defaults to 500ms (1/2 second)
-    //        set
-    button.on("hold", function() {
-        // console.log("hold");
-    });
-
-    // "up" the button is released
-    button.on("up", function() {
-        // console.log("up");
-    });
-
-    button2.on("down", function() {
-        console.log("button 2 down");
-        digitalLed1.toggle();
-        digitalLed2.toggle();
-    });
-
-    // "hold" the button is pressed for specified time.
-    //        defaults to 500ms (1/2 second)
-    //        set
-    button2.on("hold", function() {
-        console.log("button 2 hold");
-    });
-
-    // "up" the button is released
-    button2.on("up", function() {
-        console.log("button 2 up");
-    });
-
-    button3.on("down", function() {
-        console.log("button 3 down");
-    });
-    button3.on("hold", function() {
-        console.log("button 3 hold");
-    });
-    button3.on("up", function() {
-        console.log("button 3 up");
-    });
-
-    button4.on("down", function() {
-        console.log("button 4 down");
-    });
-    button4.on("hold", function() {
-        console.log("button 4 hold");
-    });
-    button4.on("up", function() {
-        console.log("button 4 up");
-    });
-
-    button5.on("down", function() {
-        console.log("button 5 down");
-    });
-    button5.on("hold", function() {
-        console.log("button 5 hold");
-    });
-    button5.on("up", function() {
-        console.log("button 5 up");
-    });
-
-    // let isAnaloged = false;
-    // analogInput.on('data', function() {
-    //     if (!isAnaloged) {
-    //         console.log('data is coming!');
-    //         console.log(this);
-    //         isAnaloged = true;
-    //     }
-    // })
-
-
-    // digitalLed1.strobe();
-    // digitalLed2.strobe();
-    // digitalLed1.blink();
-    // digitalLed2.blink();
-
-    // (new five.Led('P1-7').Led('P1-8')).strobe();
-     */
-
 });
--- playground/ui.js
+++ playground/ui.js
@@ -4,6 +4,92 @@ const mainMenu = [
     "change time",
     "exit"
 ];
+
+module.exports = (todos, setting) => {
+
+    const pomoAction = () => {
+        if (!setting.isStarted) {
+            console.log('start!');
+            setting.isStarted = true;
+        } else {
+            console.log('stop!');
+            setting.isStarted = false;
+        }
+    };
+
+    const onExit = () => {
+        console.log('exit!');
+    };
+
+    const changeTime = (offset, setting) => {
+        console.log('changing time!');
+        // setting.time += offset;
+    };
+
+    const changeTask = () => {
+        console.log('changing task!');
+    };
+
+    class UINode {
+        constructor(func = undefined, children = undefined, parent = this) {
+            this.func = func;
+            this.children = children;
+            this.parent = parent;
+        }
+    }
+
+    class UI {
+        constructor(rootNode) {
+            this.state = {
+                node : rootNode,
+                select : 0,
+                isStarted : false
+            };
+        }
+
+        changeSelect(offset) {
+            const nextSelect = this.state.select + offset;
+            if (nextSelect < 0 || nextSelect >= this.state.node.children.length) return;
+            this.state.select = nextSelect;
+        }
+
+        onSelect() {
+            if (this.state.node.children) {
+                console.log('NOT nothing!');
+                this.state.node = this.state.node.children[this.state.select];
+                this.state.select = 0;
+            } else {
+                console.log('nothing!');
+            }
+        }
+
+        onBack() {
+            this.state.node = this.state.node.parent;
+            this.state.select = 0;
+        }
+
+        onAction() {
+            if (this.state.node)
+                this.state.node.func();
+        }
+    }
+
+    const printMenu = (menu) => menu.forEach((m, idx) => console.log(`${idx + 1} : ${m}`));
+
+    let root = new UINode(() => printMenu(mainMenu), []);
+    let startNode = new UINode(pomoAction, undefined, root); // leaf
+    let changeTaskNode = new UINode(changeTask, undefined, root); // leaf
+    let changeTimeNode = new UINode(changeTime, undefined, root); // leaf
+    let exitNode = new UINode(onExit, undefined, root);
+    // console.log('startnode children : ', startNode.children);
+    root.children.push(startNode);
+    root.children.push(changeTaskNode);
+    root.children.push(changeTimeNode);
+    root.children.push(exitNode);
+
+    return new UI(root);
+};
+
 //
 // let changeTaskMenu = []; // dynamically change it.
 //
@@ -19,113 +105,4 @@ const mainMenu = [
 // ];
 
 
-// leaves.
-const startPomo = () => {
-    console.log('start!');
-};
-
-const stopPomo = () => {
-    console.log('stop!');
-};
-
-const onExit = () => {
-    console.log('exit!');
-};
-
-const changeTime = (offset, setting) => {
-    console.log('changing time!');
-    // setting.time += offset;
-};
-
-const changeTask = () => {
-    console.log('changing task!');
-};
-
-class UINode {
-    constructor(func = undefined, children = undefined, parent = this) {
-        this.func = func;
-        this.children = children;
-        this.parent = parent;
-    }
-}
-
-class UI {
-    constructor(rootNode) {
-        this.state = {
-            node : rootNode,
-            select : 0,
-            isStarted : false
-        };
-    }
-
-    changeSelect(offset) {
-        const nextSelect = this.state.select + offset;
-        if (nextSelect < 0 || nextSelect >= this.state.node.children.length) return;
-        this.state.select = nextSelect;
-
-        // if (!(this.state.select > 0 || this.state.select < this.state.node.children.length - 1)) return;
-        // this.state.select += offset;
-    }
-
-    onSelect() {
-        if (this.state.node.children) {
-            console.log('NOT nothing!');
-            this.state.node = this.state.node.children[this.state.select];
-            this.state.select = 0;
-        } else {
-            console.log('nothing!');
-            // console.log()
-        }
-    }
-
-    onBack() {
-        this.state.node = this.state.node.parent;
-        this.state.select = 0;
-    }
-
-    onAction() {
-        if (this.state.node)
-            this.state.node.func();
-    }
-}
-
-const printMenu = (menu) => menu.forEach((m, idx) => console.log(`${idx + 1} : ${m}`));
-//
-// const getRootNode = () => {
-//     let root = new UINode(() => printMenu(mainMenu));
-//     let startNode = new UINode(startPomo, [], root); // leaf
-//     let changeTaskNode = new UINode(changeTask, [], root); // leaf
-//     let changeTimeNode = new UINode(changeTime, [], root); // leaf
-//     let exitNode = new UINode(onExit, [], root);
-//
-//     root.children.push(startNode);
-//     root.children.push(changeTaskNode);
-//     root.children.push(changeTimeNode);
-//     root.children.push(exitNode);
-//     //
-//     // return new UINode(
-//     //     () => printMenu(mainMenu),
-//     //     [
-//     //         new UINode(startPomo, [])
-//     //
-//     //     ]
-//     //
-//     //
-//     //
-//     // );
-// };
-
-let root = new UINode(() => printMenu(mainMenu), []);
-let startNode = new UINode(startPomo, undefined, root); // leaf
-let changeTaskNode = new UINode(changeTask, undefined, root); // leaf
-let changeTimeNode = new UINode(changeTime, undefined, root); // leaf
-let exitNode = new UINode(onExit, undefined, root);
-
-console.log('startnode children : ', startNode.children);
-
-root.children.push(startNode);
-root.children.push(changeTaskNode);
-root.children.push(changeTimeNode);
-root.children.push(exitNode);
-
-module.exports = new UI(root);
+// module.exports = new UI(root);
