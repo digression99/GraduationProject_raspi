f383e96835958602087489eed5a162e4afa97c2e
Kim Il Sik <kimilsik@Kims-MacBook.local>
2018-01-17 17:22:47 +0900

    expander test 2

--- playground/expander-test.js
+++ playground/expander-test.js
@@ -11,7 +11,7 @@ board.on('exit', function() {
 
 board.on('ready', function() {
     console.log('board is ready!');
-    let button1 = new five.Button('P1-13');
+    // let button1 = new five.Button('P1-13');
     // let led1 = new five.Led('P1-19');
 
     let virtual = new five.Board.Virtual(
@@ -23,29 +23,4 @@ board.on('ready', function() {
     });
 
     led1.on();
-
-
-    // let virtual = new five.Board.Virtual({
-    //         controller: "MCP23008"
-    //         // address: 0x??
-    //     });
-
-
-    // this.repl.inject({
-    //     button1,
-    //     led1
-    // });
-    //
-    // button1.on('down', () => {
-    //     console.log('button down!');
-    //     led1.on();
-    // });
-    // button1.on('hold', () => {
-    //     console.log('button hold!');
-    // });
-    // button1.on('up', () => {
-    //     console.log('button up!');
-    //     led1.off();
-    // });
-
 });
