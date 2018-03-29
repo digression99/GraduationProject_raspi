75cffffe39d73729c82bc7e8a42c0dd4be0e7f76
Kim Il Sik <kimilsik@Kims-MacBook.local>
2018-03-29 17:44:31 +0900

    camera fix

--- index.js
+++ index.js
@@ -12,23 +12,6 @@ const uuidv4 = require('uuid/v4');
 // load the environment.
 dotenv.load({path : '.env.development'});
 
-// camera
-const opts = {
-    width : 640,
-    height : 480,
-    mode : 'photo',
-    awb : 'off',
-    encoding : 'jpg',
-    output : `${process.env.IMAGE_FOLDER_NAME}/${uuidv4()}.jpg`,
-    q : 50, // quality
-    // timeout : 8000, // total shot time.
-    // timelapse : 400, // time between every shots.
-    // nopreview : true,
-    // th : '0:0:0'
-};
-
-const camera = new RaspiCam(opts);
-
 camera.on('exit', function () {
     camera.stop();
     console.log('camera exit.');
@@ -48,6 +31,23 @@ board.on('ready', function() {
 
     button1.on('press', function () {
         console.log('button 1 pressed.');
+
+// camera
+        const opts = {
+            width : 640,
+            height : 480,
+            mode : 'photo',
+            awb : 'off',
+            encoding : 'jpg',
+            output : `${process.env.IMAGE_FOLDER_NAME}/${uuidv4()}.jpg`,
+            q : 50, // quality
+            // timeout : 8000, // total shot time.
+            // timelapse : 400, // time between every shots.
+            // nopreview : true,
+            // th : '0:0:0'
+        };
+
+        const camera = new RaspiCam(opts);
         camera.start();
     });
 
