3ac8898ae547d7e36aaf0dc9b75560c4c36f732c
Kim Il Sik <kimilsik@Kims-MacBook.local>
2018-05-24 13:13:33 +0900

    code refactoring 1

--- config/aws.js
+++ config/aws.js
@@ -2,15 +2,21 @@
 const AWS = require('aws-sdk');
 // const dotenv = require('dotenv');
 
+const {
+    AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY,
+    AWS_REGION
+} = process.env;
+
+
 AWS.config.update({
-    accessKeyId : process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey : process.env.AWS_SECRET_ACCESS_KEY,
-    region : process.env.AWS_REGION
+    accessKeyId : AWS_ACCESS_KEY_ID,
+    secretAccessKey : AWS_SECRET_ACCESS_KEY,
+    region : AWS_REGION
 });
 
 // Create an S3 client
 const S3 = new AWS.S3();
-// const rekognition = new AWS.Rekognition();
 
 console.log('aws setting complete.');
 
